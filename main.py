f31fee9d7b589ee984521851f8d16b8a0e6c6411
Str1kez <vazinov@yandex.ru>
2022-05-24 04:27:48 +0300

    comparison on other amount of division was added

--- main.py
+++ main.py
@@ -3,31 +3,52 @@ import math
 import numpy as np
 import plotly.express as px
 import pandas as pd
+from typing import Iterable
 
 from numerical_methods import taylor, l, central, left, trapezoid, gauss
 
 
-def draw_graph(x, y, yaxis_title: str):
-    fig = px.line(x=x, y=y)
-    fig.update_layout(yaxis_title=yaxis_title)
+def draw_graph(x: Iterable = None, y: Iterable = None, yaxis_title: str = None, data_frame: pd.DataFrame = None):
+    fig = px.line(data_frame=data_frame) if data_frame is not None else px.line(x=x, y=y)
+    if yaxis_title:
+        fig.update_layout(yaxis_title=yaxis_title)
     fig.show()
 
 
+def draw_complex_comparison() -> None:
+    chebyshev = pd.read_csv('data/max_eps_chebyshev.csv')
+    uniform = pd.read_csv('data/max_eps_uniform.csv')
+    union_df = pd.merge(chebyshev, uniform)
+    union_df.index = union_df.pop('Узлы')
+    draw_graph(data_frame=union_df, yaxis_title='Max_n(eps)')
+    
+
+def draw_slow_comparison() -> None:
+    comp_df = pd.read_csv('data/max_eps_uniform_slow.csv')
+    comp_df.index = comp_df.pop('Узлы')
+    draw_graph(data_frame=comp_df, yaxis_title='Max_n(eps)')
+
+
 def derivative_complex() -> None:
-    x = np.linspace(a, b, 3 * n)  # 20 точек
+    x = np.linspace(a, b, 2 * n)  # 20 точек
     gt = np.cos(np.pi * x ** 2 / 2)  # подынтегральная функция
-    data = {el: {} for el in x}
-    for i in range(n, 50, 2):
-        nodes = np.linspace(a, b, i)  # узлы разбиения
+    max_dev = []
+    max_n = 30
+    step = 1
+    for i in range(n, max_n, step):
+        nodes = np.linspace(a, b, i)  # узлы разбиения равноотдаленные
+        # узлы Чебышёва
+        # nodes = (a + b) / 2 + (b - a) / 2 * np.cos([np.pi * (2 * k - 1) / 2 / i for k in range(1, i + 1)])
         f = [taylor(x, e) for x in nodes]  # Fn
+        x_dev = []
         for point in x:  # расчет для фиксированных точек
-            L = 0
-            for k in range(i):
-                L += f[k] * l(point, k, nodes)
-            data[point][i] = L
-        x_dev = np.array([data[x][i] for x in data])  # значения в фиксированных точках при разбиении i
-        print(np.max(np.abs(x_dev - gt)), f'Узлов: {i}')  # Погрешность
-    json.dump(data, open('.result.json', 'w'), indent=4)
+            L = sum(f[k] * l(point, k, nodes) for k in range(i))
+            x_dev.append(L)
+        x_dev = np.array(x_dev)  # значения в фиксированных точках при разбиении i
+        max_dev.append(np.max(np.abs(x_dev - gt)))  # Погрешность
+    derivative_df = pd.DataFrame(data={'Max(eps)': max_dev}, index=pd.Index(data=range(n, max_n, step), name='Узлы'))
+    derivative_df.to_csv('data/max_eps_uniform_slow.csv')
+    # draw_graph(data_frame=derivative_df, yaxis_title='Погрешность')
     
 
 def derivative_single_analysis() -> None:
@@ -54,7 +75,7 @@ def derivative_single_analysis() -> None:
     # derivative_df.to_csv('data/derivative.csv')
     derivative_df.to_csv('data/derivative_chebyshev.csv')
 
-    draw_graph(x, gt, yaxis_title="С'(x)")
+    draw_graph(x=x, y=gt, yaxis_title="С'(x)")
 
 
 def tabulation_analysis() -> None:
@@ -67,7 +88,7 @@ def tabulation_analysis() -> None:
     }, index=pd.Index(data=x, name='xi'))
     tabulation_df.to_csv('data/tabulation.csv')
 
-    draw_graph(x, [p[0] for p in f], 'С(xi)')
+    draw_graph(x=x, y=[p[0] for p in f], yaxis_title='С(xi)')
 
 
 def quadrature_analysis(quad_func, count: int = None) -> None:
@@ -102,10 +123,13 @@ if __name__ == '__main__':
     b = 1.5
     h = .15
     n = 10
-    e = math.pow(10, -6)
-    tabulation_analysis()
-    derivative_single_analysis()
-    quadrature_analysis(gauss)
-    quadrature_analysis(central)
-    quadrature_analysis(left)
-    quadrature_analysis(trapezoid)
+    e = math.pow(10, -4)
+    # tabulation_analyis()
+    # derivative_complex()
+    draw_complex_comparison()
+    draw_slow_comparison()
+    # derivative_single_analysis()
+    # quadrature_analysis(gauss)
+    # quadrature_analysis(central)
+    # quadrature_analysis(left)
+    # quadrature_analysis(trapezoid)
